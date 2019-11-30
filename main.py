95b19f5103ce92ce6c8594de1d431e7d0cc0aa1a
KoolKidAstrid <bloodstreamer1212@gmail.com>
2019-11-30 12:16:20 -0600

    added reading an external file for token.  thanks safety jim

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ import discord
 import random
 import asyncio
 
+
+tokenfile = 'C:\\Users\\Astrid\\code\\MEL token\\meltoken.txt'
+t = open(tokenfile, 'r')
+print(t.readline())
+
 client = discord.Client()
+token = t.readline()
+
+classes = ['nothing', 'fighter', ]
 
 @client.event
 async def on_ready():
     await client.change_presence(activity=discord.Game(name='AAAAAAAAAAAAAAAAAAAAAAA'))
+    print('Ready!')
 
 @client.event
 async def on_message(message):
@@ -23,6 +32,18 @@ async def on_message(message):
                 else:
                     await message.channel.send("AAAAAAAAAAAAAAAA")
             elif str(args[0]).lower() == 'rpg':
+                f = open('users.txt', 'r+')
+                textOutput = f.read().splitlines()
+                f.close
+                startnum = textOutput.index(str(message.author.id))
+                level = int(textOutput[startnum + 4])
+                xp = int(textOutput[startnum + 6])
+                name = textOutput[startnum + 2]
+                money = int(textOutput[startnum + 8])
+                charclass = int(textOutput[startnum + 10])
+                hp = int(textOutput[startnum + 12])
+                maxhp = int(textOutput[startnum + 14])
+                items = []
                 if len(args) == 1:
                     await message.channel.send('Try "!rpg help"')
                 if str(args[1]).lower() == 'help':
@@ -36,7 +57,7 @@ async def on_message(message):
                     await message.channel.send(possibleResponses[temp])
                 if str(args[1]).lower() == 'name':
                     await message.channel.send('Mechanical Erratic Liberal (M.E.L.)')
-        #await message.channel.send(first)
+#        await message.channel.send(first)
 
 async def my_background_task():
     await client.wait_until_ready()
@@ -44,7 +65,7 @@ async def my_background_task():
         f = open('users.txt', 'r+')
         textOutput = f.read().splitlines()
         f.close
-        print(textOutput)
+#        print(textOutput)
         for member in client.get_all_members():
             if member.id != 643635275230216212:
                 if str(member.id) in textOutput:
@@ -53,7 +74,7 @@ async def my_background_task():
                     xp = int(textOutput[startnum+6])
                     name = textOutput[startnum+2]
                     money = int(textOutput[startnum+8])
-                    charclass = textOutput[startnum+10]
+                    charclass = int(textOutput[startnum+10])
                     hp = int(textOutput[startnum + 12])
                     maxhp = int(textOutput[startnum + 14])
                     items = []
@@ -61,11 +82,13 @@ async def my_background_task():
                     if xp >= (level^2 * 100):
                         xp -= (level^2 * 100)
                         level += 1
+                        user = client.get_user(member.id)
+                        await user.send(':confetti_ball: Congrats!  You leveled up! :confetti_ball:')
 
-                    print (name, str(level), str(xp), charclass, str(money), str(hp)+'/'+str(maxhp))
+#                     print (name, str(level), str(xp), charclass, str(money), str(hp)+'/'+str(maxhp))
                 else:
-                    textOutput.extend(('id', (str(member.id)), 'name', member.name, 'level', '1', 'xp', '0', 'money', '500', 'class', 'fighter', 'hp', '10', 'maxhp', '10', 'items', ''))
-        #print(textOutput)
+                    textOutput.extend(('id', (str(member.id)), 'name', member.name, 'level', '1', 'xp', '0', 'money', '500', 'class', '0', 'hp', '10', 'maxhp', '10', 'items', ''))
+#        print(textOutput)
         f = open('users.txt', 'w+')
         f.truncate()
         for line in textOutput:
@@ -74,4 +97,4 @@ async def my_background_task():
         await asyncio.sleep(1)
 
 client.loop.create_task(my_background_task())
-client.run("NjQzNjM1Mjc1MjMwMjE2MjEy.XdbCQg.e0wdddowBRftKC2pohFgV1FTjfw")
+client.run(token)
